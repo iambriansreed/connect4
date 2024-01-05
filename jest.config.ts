a9f18de73d8dd27537f98f6f8c29cf6a7bdb111d
Brian Reed <iambriansreed@gmail.com>
2024-01-05 00:53:05 -0500

    cleanup jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,6 @@
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-        // process `*.tsx` files with `ts-jest`
-    },
     moduleNameMapper: {
         '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
     },
