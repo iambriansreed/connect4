77e2c56a922fe924a13da359a8c4752726b2206f
Brian Reed <iambriansreed@gmail.com>
2024-01-27 23:14:56 -0500

    ver, bg

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import dns from 'dns';
-
-dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
-    base: '',
-    build: {
-        target: 'es2015',
-        outDir: 'build',
-    },
     server: {
         port: 4444,
     },
-    // omit
 });
