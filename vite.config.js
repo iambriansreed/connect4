04e5c530411a4c6132085ce49a1cf24e249706aa
Brian Reed <iambriansreed@gmail.com>
2024-06-17 21:36:44 -0400

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@
 import { defineConfig } from 'vite';
 import fs from 'node:fs';
 
+// https://vitejs.dev/config/
 export default defineConfig(() => ({
     server: {
-        host: 'localhost',
         port: 4444,
         https: {
             key: fs.readFileSync('./.cert/key.pem'),
