9a9113336b9b1e725d7bec29fca557eda2bf4860
Brian Reed <iambriansreed@gmail.com>
2024-06-17 21:23:12 -0400

    favicon, cleanup

--- postbuild.js
+++ postbuild.js
@@ -1,4 +1,3 @@
-// typescript
 import fs from 'node:fs';
 import path from 'node:path';
 
@@ -11,4 +10,15 @@ fs.readdirSync(dir, { recursive: true, withFileTypes: true }).forEach((file) => 
     if (!file.isFile()) return;
     const filePath = path.resolve(file.parentPath, file.name);
     fs.writeFileSync(filePath, fs.readFileSync(filePath, 'utf8').replace(/%version%/g, version));
+});
+
+const from = 'public';
+const to = 'dist';
+
+fs.readdirSync(from).forEach((element) => {
+    if (fs.lstatSync(path.join(from, element)).isFile()) {
+        fs.copyFileSync(path.join(from, element), path.join(to, element));
+    } else {
+        copyFolderSync(path.join(from, element), path.join(to, element));
+    }
 });
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import fs from 'node:fs';
-const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 export default defineConfig(() => ({
     server: {
